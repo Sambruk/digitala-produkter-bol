f33f0f7bec7c3fd8d7cd6998fdc94550856274d9
MichaelaVesterlund <70258768+MichaelaVesterlund@users.noreply.github.com>
2024-11-26 11:53:42 +0100

    Updated readme and added a json file with the specifiction for BoL version one.

--- Statistik Metod 2 Anrop med organisationsnummer.js
+++
@@ -1,5 +0,0 @@
-{
-	serviceProviderId: "ne.se",
-	clientId: "goteborgsregionen.se",
-	organizationNumbers: ["5589028532", "21200212134", "2021254623458"]
-}
--- Statistik Metod 2 Anrop med skolenhetskoder.js
+++
@@ -1,5 +0,0 @@
-{
-	serviceProviderId: "ne.se",
-	clientId: "goteborgsregionen.se",
-	schoolUnitCodes: ["1231567", "8564534", "8565434"]
-}
